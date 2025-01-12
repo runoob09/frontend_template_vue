c58bb174f59c670d2aa68436233c377c5ec2f24e
runoob09 <2499469495@qq.com>
2025-01-13 00:06:03 +0800

    feature: 实现了登录表单

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,36 +1,37 @@
 <script setup lang="ts">
-import {reactive} from "vue";
+import { reactive } from "vue";
+import background from '@/assets/background.svg';
 
 const loginData = reactive({
-  userAccount:"",
-  userPassword:""
-})
+  userAccount: "",
+  userPassword: ""
+});
 </script>
 
 <template>
-  <div class="pannel">
-    <a-form :model="loginData" :style="{ width: '600px' }" @submit="handleSubmit">
-      <a-form-item field="name" tooltip="Please enter username" label="Username">
-        <a-input
-            v-model="form.name"
-            placeholder="please enter your username..."
-        />
-      </a-form-item>
-      <a-form-item field="post" label="Post">
-        <a-input v-model="form.post" placeholder="please enter your post..." />
-      </a-form-item>
-      <a-form-item field="isRead">
-        <a-checkbox v-model="form.isRead"> I have read the manual </a-checkbox>
-      </a-form-item>
-      <a-form-item>
-        <a-button html-type="submit">Submit</a-button>
-      </a-form-item>
-    </a-form>
+  <div class="flex justify-center items-center h-screen" :style="{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }">
+    <div class="panel">
+      <a-form :model="loginData" size="large" auto-label-width :style="{ width: '400px' }">
+        <a-form-item field="userAccount" tooltip="请输入账号" label="账号">
+          <a-input v-model="loginData.userAccount" placeholder="请输入账号..." />
+        </a-form-item>
+        <a-form-item field="userPassword" tooltip="请输入密码" label="密码">
+          <a-input v-model="loginData.userPassword" placeholder="请输入密码..." type="password"/>
+        </a-form-item>
+        <a-form-item class="no-bottom-margin">
+          <a-button html-type="submit">Submit</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.pannel{
-  @apply
+<style>
+.panel {
+  @apply w-auto h-auto border border-slate-100 shadow-md p-24 inline-block;
+}
+
+.no-bottom-margin {
+  margin-bottom: 0 !important;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite';
+import {ArcoResolver} from 'unplugin-vue-components/resolvers';
+import { vitePluginForArco } from '@arco-plugins/vite-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ArcoResolver()],
+        }),
+        Components({
+            resolvers: [
+                ArcoResolver({
+                    sideEffect: true
+                })
+            ]
+        }),
+        vitePluginForArco({
+            style: 'css'
+        })
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'), // 将 @ 指向 src 目录
